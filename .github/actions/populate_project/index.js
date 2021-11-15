f3868e1eea3d70aac86c3cbcb66b81b4b7e059a9
Andy Feller <andyfeller@github.com>
2021-11-15 14:47:41 -0500

    Add initial support for populating issues

--- .github/actions/populate_project/index.js
+++ .github/actions/populate_project/index.js
@@ -1,5 +1,6 @@
 // Define constants used for contracts / IO
 const INPUT_CONFIG_DIR = 'config-dir';
+const INPUT_DEBUG = 'debug';
 const INPUT_FAIL_FAST = 'fail-fast';
 const INPUT_GITHUB_TOKEN = 'github-token';
 
@@ -9,68 +10,114 @@ const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
+const util = require('util');
+const handlerbars = require('handlebars');
 
 async function run() {
 
-    // Verify inputs for GitHub API interactions
-    const githubToken = core.getInput(INPUT_GITHUB_TOKEN, { required: true });
-    const octokit = github.getOctokit(githubToken);
     const context = github.context;
-    console.dir(context);
+
+    if (debug) {
+        console.debug(`GitHub context: ${util.inspect(context)}`);
+    }
 
     // Verify inputs for action configuration
+    const debug = core.getBooleanInput(INPUT_DEBUG);
     const failFast = core.getBooleanInput(INPUT_FAIL_FAST);
     const configDir = core.getInput(INPUT_CONFIG_DIR, { required: true });
+    const githubToken = core.getInput(INPUT_GITHUB_TOKEN, { required: true });
+
+    // Verify and process configuration file
+    const octokit = github.getOctokit(githubToken);
     const configPath = path.join(configDir, 'config.yml')
 
     fs.access(configPath, fs.constants.R_OK, async (err) => {
 
         if (err) {
-            // TODO: Log information about what this error means
             throw err;
         }
 
         // Create supported resources For project(s) configured
-        const config = yaml.load(fs.readFileSync(configPath));
         let success = true;
+        const config = yaml.load(fs.readFileSync(configPath));
+        const projectConfigs = config?.projects ?? [];
 
-        for (const project of config.projects) {
+        for (const projectConfig of projectConfigs) {
 
             try {
                 // Create project
-                const { data: createForRepoResponse } = await octokit.rest.projects.createForRepo({
+                const { data: project } = await octokit.rest.projects.createForRepo({
                     ...context.repo,
-                    name: project.name,
-                    body: project.body,
+                    name: projectConfig.name,
+                    body: projectConfig.body,
                 });
 
-                console.log(`Created project "${createForRepoResponse['name']}" (${createForRepoResponse['id']})"`);
+                console.log(`Created project "${project.name}" (${project.id})`);
 
                 // Create project column(s)
-                for (const column of project.columns) {
-                    const { data: createColumnResponse } = await octokit.rest.projects.createColumn({
-                        project_id: createForRepoResponse['id'],
-                        name: column
+                const columnConfigs = projectConfig?.columns ?? [];
+                const columnNameLookup = {};
+                let defaultColumn = null;
+
+                for (const columnConfig of columnConfigs) {
+                    const { data: column } = await octokit.rest.projects.createColumn({
+                        project_id: project.id,
+                        name: columnConfig
                     })
 
-                    console.log(`Created project column "${createColumnResponse['name']}" (${createColumnResponse['id']})" for project "${createForRepoResponse['name']}" (${createForRepoResponse['id']})`);
+                    console.log(`Created project column "${column.name}" (${column.id}) for project "${project.name}" (${project.id})`);
+
+                    columnNameLookup[column.name] = column;
+
+                    if (defaultColumn == null) {
+                        defaultColumn = column;
+                    }
                 }
 
                 // Create project issues
-                // TODO: Need templating integration
+                const issueConfigs = projectConfig?.issues ?? [];
+                const templateData = {
+                    github: context,
+                    project: project,
+                }
+
+                for (const issueConfig of issueConfigs) {
+
+                    // Create issue based on templated content
+                    const issueTitle = handlerbars.template(issueConfig.title);
+                    const issueBody = handlerbars.template(issueConfig.body);
+                    const column = columnNameLookup[issueConfig?.column] ?? defaultColumn;
+
+                    const { data: issue } = await octokit.rest.issues.create({
+                        ...context.repo,
+                        title: issueTitle(templateData),
+                        body: issueBody(templateData),
+                    })
+
+                    console.log(`Created issue "${issue.title}" (${issue.id}) for project "${project.name}" (${project.id})`);
+
+                    // Create project card, associates issue to project column
+                    const { data: card } = await octokit.rest.projects.createCard({
+                        column_id: column.id,
+                        content_type: 'Issue',
+                        content_id: issue.id,
+                    })
+
+                    console.log(`Created project card (${card.id}) to ${card.content_type} (${card.content_id}) for project "${project.name}" (${project.id})`);
+                }
             } catch (err) {
                 success = false;
 
                 if (failFast) {
                     throw err;
                 } else {
-                    console.log(err);
+                    console.error(err);
                 }
             }
         }
 
         if (!success) {
-            throw new Error('One or more errors were caught while creating projects without fast-fail');
+            throw new Error(`One or more errors were caught while creating projects without ${INPUT_FAIL_FAST}`);
         }
     })
 }
